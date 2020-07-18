59545ecf7316e74354def520dd374849c2be3a63
tglide <thomglopes@gmail.com>
2020-07-18 20:15:26 -0300

    :heavy_plus_sign: add hot reload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImagesController } from './images/images.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ImagesController],
   providers: [AppService],
 })
 export class AppModule {}
